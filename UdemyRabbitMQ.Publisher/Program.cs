25df8bd8d82c622931d5e7fc96692012e62b36c2
aenesyvz <20060403@stu.omu.edu.tr>
2024-07-31 10:41:19 +0300

    basic publisher and consumer example

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+//BasicUsage(channel);
+
+SendMultipleMessages(channel);
+
+Console.ReadLine();
+
+static void BasicUsage(IModel channel)
+{
+    string message = "hello world";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+    Console.WriteLine("Send Message!");
+}
+
+
+static void SendMultipleMessages(IModel channel)
+{
+    Enumerable.Range(1, 50).ToList().ForEach(x =>
+    {
+
+        string message = $"Message {x}";
+
+        var messageBody = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+        Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+
+    });
+}
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+//BasicUsage(channel);
+
+ReceiveMultipleMessage(channel);
+
+Console.ReadLine();
+
+
+
+
+static void BasicUsage(IModel channel)
+{
+    var consumer = new EventingBasicConsumer(channel);
+
+    channel.BasicConsume("hello-queue", true, consumer);
+
+
+    consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+        Console.WriteLine("Received Message : " + message);
+    };
+}
+
+static void ReceiveMultipleMessage(IModel channel)
+{
+    channel.BasicQos(0, 1, false);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    channel.BasicConsume("hello-queue", false, consumer);
+
+
+
+    consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Thread.Sleep(1500);
+        Console.WriteLine("Gelen Mesaj:" + message);
+
+        channel.BasicAck(e.DeliveryTag, false);
+    };
+}
